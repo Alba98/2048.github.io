c9d852325223b84026d1eb3b98a6358ed82d2dca
Alba <albatxu98am@gmail.com>
2022-10-20 13:08:13 +0200

    movimiento fichas

--- js/script.js
+++ js/script.js
@@ -9,10 +9,10 @@ var columnas = 4;
 window.onload = function() { iniciarJuego(); }
 
 function iniciarJuego() {
-    tablero = [ [2,2,2,2], 
-                [2,2,2,2], 
-                [2,2,2,2],
-                [2,2,2,2]  ];
+    tablero = [ [0, 0, 0, 0], 
+                [0, 0, 0, 0], 
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]  ];
     
     for (let y = 0; y < filas; y++) {
         for (let x = 0; x < columnas; x++) {
@@ -38,17 +38,17 @@ function actualizarFicha(ficha, num) {
 }
 
 document.addEventListener('keyup', (e) => {
-    if (e.code == "ArrowLeft" || e.code == "a") {
+    if (e.code == "ArrowLeft" || e.code == "KeyA") {
         deslizarIzq();
     }
-    else if (e.code == "ArrowRight" || e.code == "d") {
-        //deslizarDch();
+    else if (e.code == "ArrowRight" || e.code == "KeyD") {
+        deslizarDch();
     }
-    else if (e.code == "ArrowUp" || e.code == "w") {
-        //deslizarArriba();
+    else if (e.code == "ArrowUp" || e.code == "KeyW") {
+        deslizarArriba();
     }
-    else if (e.code == "ArrowDown" || e.code == "s" ) {
-        //deslizarAbajo();
+    else if (e.code == "ArrowDown" || e.code == "KeyS" ) {
+        deslizarAbajo();
     }
     document.getElementById("score").innerText = score;
 })
@@ -86,6 +86,49 @@ function deslizarIzq() {
         for (let x = 0; x < columnas; x++){
             let tile = document.getElementById(y.toString() + "-" + x.toString());
             let num = tablero[y][x];
+            actualizarFicha(tile, num);
+        }
+    }
+}
+
+function deslizarDch() {
+    for (let y = 0; y < filas; y++) {
+        let fila = tablero[y];
+        fila.reverse();
+        fila = deslizar(fila);
+        fila.reverse();
+        tablero[y] = fila;
+        for (let x = 0; x < columnas; x++){
+            let tile = document.getElementById(y.toString() + "-" + x.toString());
+            let num = tablero[y][x];
+            actualizarFicha(tile, num);
+        }
+    }
+}
+
+function deslizarArriba() {
+    for (let c = 0; c < columnas; c++) {
+        let fila = [tablero[0][c], tablero[1][c], tablero[2][c], tablero[3][c]];
+        fila = deslizar(fila);
+        for (let r = 0; r < filas; r++){
+            tablero[r][c] = fila[r];
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = tablero[r][c];
+            actualizarFicha(tile, num);
+        }
+    }
+}
+
+function deslizarAbajo() {
+    for (let c = 0; c < columnas; c++) {
+        let fila = [tablero[0][c], tablero[1][c], tablero[2][c], tablero[3][c]];
+        fila.reverse();
+        fila = deslizar(fila);
+        fila.reverse();
+        for (let r = 0; r < filas; r++){
+            tablero[r][c] = fila[r];
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = tablero[r][c];
             actualizarFicha(tile, num);
         }
     }
